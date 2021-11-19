3fc7df5790c50784a03a565943a60332d970b624
1170500704 <706461857@qq.com>
2021-11-19 20:37:27 +0800

    前后端结合
    导诊项目完成了一些，还有一些牛马界面

---
+++ src/main/java/com/whu/demo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.whu.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping(value = "/test")
+    public String say(){
+        return "hello";
+    }
+}
---
+++ src/main/java/com/whu/demo/dao/Test.java
@@ -0,0 +1,12 @@
+package com.whu.demo.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Neo4jDao neo4jDao=new Neo4jDao();
+        String a=neo4jDao.getAnswer1();
+        System.out.println(a);
+    }
+}
